c77f3cf6037dd88ae5521248efaf0f5c15a8befd
Yuvaraj Ilangovan <yilangovan@agiletrailblazers.com>
2015-11-04 11:58:10 -0500

    Fix for build Failure

--- src/test/java/com/cucumber/GSWebsiteATDD/RunnerTest.java
+++ src/test/java/com/cucumber/GSWebsiteATDD/RunnerTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(
-        features = {"src/test/resource/feature"}
+        features = "src/test/resource/feature", tags={"@contactusPageForm, @feedbackForm, @aboutUsPage"}
 )
 public class RunnerTest {
 
